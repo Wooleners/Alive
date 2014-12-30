b2c9a35934fb0587dd9534859ddb6801f51047cf
wooleners <wooleners@wooleners-mac.local>
2014-12-30 18:30:52 +0800

    rm some

--- src/app.js
+++ src/app.js
@@ -5,21 +5,3 @@ angular.module('annuityApp', [
   'annuityApp.module1',
   'annuityApp.module2'
 ]);
-
-// (function(document,$,angular){
-//   angular.element(document).ready(function() {
-//     $.ajax({
-//       url: '/api/get_user_permission',
-//       type: "GET",
-//       dataType: 'json'
-//     }).then(function(data){
-//         for (var i = 0; i < data.permissions.length; i++) {
-//           data.permissions[i] = data.permissions[i].replace(/\s/g,"");
-//         };
-//         angular.module('annuityApp').run(['$rootScope', function($rootScope){
-//           $rootScope = data.permissions;
-//         }]);
-//         angular.bootstrap(document, ['annuityApp']);
-//     });
-//   });
-// })(document,jQuery,angular);
